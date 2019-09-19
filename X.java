60c8dcd47597c0849ccd677995a0f4eff2b118af
deepashammi <54535695+deepashammi@users.noreply.github.com>
2019-09-20 01:07:55 +0530

    Create X.java

---
+++ X.java
@@ -0,0 +1,15 @@
+package a.b
+class x(){
+public void y(){
+system.out.println("first method called")
+}
+public void z(){
+system.out.println("second method called")
+}
+
+public static void main(String args[]){
+x obj = new x();
+obj.y();
+obj.z();
+}
+}
